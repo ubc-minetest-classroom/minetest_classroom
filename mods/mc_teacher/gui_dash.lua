16ce345371defa052bd7f1f8e831419cb45fe4cb
pauldpickell <61996662+pauldpickell@users.noreply.github.com>
2022-05-09 14:55:46 -0700

    Removed legacy reference to mc_classrooms

--- mods/mc_teacher/gui_dash.lua
+++ mods/mc_teacher/gui_dash.lua
@@ -896,7 +896,7 @@ months = {
 function place_map(player,map_name,pos)
 	local pname = player:get_player_name()
 	if check_perm(player) then
-		minetest.place_schematic(pos, minetest.get_modpath("mc_classrooms").."/maps/"..map_name..".mts", 0, nil, true)
+		minetest.place_schematic(pos, minetest.get_modpath("mc_teacher").."/maps/"..map_name..".mts", 0, nil, true)
 	else
 		minetest.chat_send_player(pname,pname..": You do not have the teacher privilege to create a new map. Check with the server administrator.")
 	end
