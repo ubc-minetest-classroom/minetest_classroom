7c51fa24aae4601169ce8476b6735ccdd950e9ce
Lukas Olson <lukasolson@greyblockgames.com>
2022-05-11 00:59:40 -0700

    Added TODO notes; renamed Create() method to new(); added Delete() method

--- mods/mc_worldmanager/realm.lua
+++ mods/mc_worldmanager/realm.lua
@@ -1,15 +1,20 @@
 -- Realms are 3,000 node * 3,000 node areas seperated by a 1,000 block border of void (in each dimension);
 -- TODO: make realm size dynamic
+-- TODO: save realm info to storage
+-- TODO: add helper functions to do stuff like teleport players into the maps
+-- TODO: add invisible world border around realms
 
-local realmSettings = { size = 3000, buffer = 1000, worldSize = 20000 }
 
-Realm = { realmCount = 0, realmList = {} }
+---@public
+---Class that manages all realms in Minetest_Classroom.
+---@class
+Realm = { realmCount = 0, realmDict = {} }
 Realm.__index = Realm
 
 ---@public
----creates a new Dimension
+---creates a new Dimension.
 ---@return self
-function Realm:Create()
+function Realm:new()
 
     local this = {
         ID = Realm.realmCount + 1,
@@ -25,23 +30,32 @@ function Realm:Create()
     realmLocation.z = Math.ceil(this.ID / 10)
 
     -- Calculate our world position based on our location on the realm grid
-    this.Position.x = -realmSettings.worldSize + (realmSettings.size * realmLocation.x) + realmSettings.buffer
-    this.Position.z = -realmSettings.worldSize + (realmSettings.size * realmLocation.z) + realmSettings.buffer
+    this.Position.x = -20000 + (3000 * realmLocation.x) + 1000
+    this.Position.z = -20000 + (3000 * realmLocation.z) + 1000
 
     -- Temporary spawn point calculation
     this.SpawnPoint = this.Position
     this.SpawnPoint.y = this.Position.y + 2
 
-    setmetatable(this, Realm)
+    Realm.realmDict[this.ID] = this
 
-    Realm.realmList = Realm.realmList + {0, this}
-
+    setmetatable(this, self)
     return this
 end
 
 ---@public
+---Deletes the realm based on class instance.
 ---@return void
 function Realm:Delete()
+    Realm.DeleteByID(self.ID)
+end
 
+---@public
+---Deletes the realm based on the supplied realm ID.
+---@param ID number
+---@return void
+function Realm.DeleteByID(ID)
+    --TODO: Clear world blocks in the realm before removing reference in the realmDICT
+    Realm.realmDict[ID] = nil
 end
 
