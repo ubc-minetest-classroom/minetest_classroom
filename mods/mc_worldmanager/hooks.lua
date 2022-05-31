ecdafc2b59536caf8064c989b22de1059ea68c4e
lukasdragon <lukasdragon@live.ca>
2022-05-30 17:27:41 -0700

    Set player respawning to the realm they're a member of.

--- mods/mc_worldmanager/hooks.lua
+++ mods/mc_worldmanager/hooks.lua
@@ -1,20 +1,28 @@
--- on player spawn, we teleport them to the world spawn.
+-- on first player spawn, we assign them to the world spawn realm
+-- and set their position to that spawnpoint .
 minetest.register_on_newplayer(function(player)
 
     local spawnRealm = mc_worldManager.GetSpawnRealm()
 
     local pmeta = player:get_meta()
-    pmeta:set_int("realm",spawnRealm.ID)
-
+    pmeta:set_int("realm", spawnRealm.ID)
 
     player:set_pos(spawnRealm.SpawnPoint)
 end)
 
--- When players respawn, we also teleport them to the world spawn.
--- This might be changed in the future to spawn the players at their own spawn;
--- could be set by travelling to other realms?
+-- When players respawn, we teleport them to the spawnpoint of the realm they belong to
 minetest.register_on_respawnplayer(function(player)
-    local spawnRealm = mc_worldManager.GetSpawnRealm()
+
+    local pmeta = player:get_meta()
+    local playerRealmID = pmeta:get_int("realm")
+
+    local spawnRealm
+
+    if (playerRealmID ~= nil) then
+        spawnRealm = Realm.realmDict[playerRealmID]
+    else
+        spawnRealm = mc_worldManager.GetSpawnRealm()
+    end
 
     player:set_pos(spawnRealm.SpawnPoint)
     return true
