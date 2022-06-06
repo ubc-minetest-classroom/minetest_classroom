679052d213d584aef72eff01908285bf7fceb5a3
Nanjou <lowie375@gmail.com>
2022-06-06 13:22:45 -0700

    Implement rudimentary toolbox
    * Add detached toolbox inventory
    * Add automatic toolbox saving/loading

--- mods/mc_toolmenu/init.lua
+++ mods/mc_toolmenu/init.lua
@@ -3,66 +3,110 @@ mc_toolmenu = minetest.get_mod_storage()
 sfinv.register_page("mc_toolmenu:tools", {
     title = "Toolbox",
     get = function(self, player, context)
+        local pname = player:get_player_name()
         local formtable = {
             "box[-0.28,-0.30;8.35,4.9;#555555]",
             "label[0,0;Soon, this will be made into a working toolbox!]",
-            "list[current_player;mc_toolmenu:tools;0,0.5;8,1;0]"
+            "list[detached:mc_toolmenu:", pname, ";tools;0,0.5;8,4;0]"
         }
         return sfinv.make_formspec(player, context, table.concat(formtable, ""), true)
     end
 })
 
--- debug
---minetest.remove_detached_inventory("mc_toolmenu:test")
+-- Saves the player's entire toolbox to mod storage
+local function save_full_toolbox(toolbox, player)
+    local toolbox_table = {}
+    local pname = player:get_player_name()
+    local lists = toolbox:get_lists()
 
-minetest.register_on_joinplayer(function(player)
-    if not player:is_player() then
-        return
+    for list,data in pairs(lists) do
+        local list_table = {}
+        for i,item in pairs(data) do
+            list_table[i] = item:to_table()
+        end
+        toolbox_table[list] = {inv = list_table, size = toolbox:get_size(list)}
     end
 
-    local pname = player:get_player_name()
-    local inv_list = mc_toolmenu:get(pname)
+    mc_toolmenu:set_string(pname, minetest.serialize(toolbox_table))
+end
 
-    inv = minetest.create_detached_inventory("mc_toolmenu:"..pname, {
+-- Loads the saved toolbox data into MineTest
+local function load_saved_toolbox(toolbox, player)
+    local pname = player:get_player_name()
+    local save = minetest.deserialize(mc_toolmenu:get_string(pname))
+
+    for list,data in pairs(save) do
+        toolbox:set_list(list, {})
+        toolbox:set_size(list, data.size)
+        for i,item in pairs(data.inv) do
+            toolbox:set_stack(list, i, ItemStack(item))
+        end
+    end
+end
+
+minetest.register_on_joinplayer(function(player)
+    -- player check
+    if not player:is_player() then return end
+
+    local pname = player:get_player_name()
+    local toolbox = minetest.create_detached_inventory("mc_toolmenu:"..pname, {
         --allow_move = function(inv, from_list, from_index, to_list, to_index, count, player),
         -- Called when a player wants to move items inside the inventory.
         -- Return value: number of items allowed to move.
 
-        --allow_put = function(inv, listname, index, stack, player),
+        allow_put = function(inv, listname, index, stack, player)
+            return 65536 -- stub
+        end,
         -- Called when a player wants to put something into the inventory.
         -- Return value: number of items allowed to put.
         -- Return value -1: Allow and don't modify item count in inventory.
 
-        --allow_take = function(inv, listname, index, stack, player),
+        allow_take = function(inv, listname, index, stack, player)
+            return 65536 -- stub
+        end,
         -- Called when a player wants to take something out of the inventory.
         -- Return value: number of items allowed to take.
         -- Return value -1: Allow and don't modify item count in inventory.
 
-        --on_move = function(inv, from_list, from_index, to_list, to_index, count, player),
-        --on_put = function(inv, listname, index, stack, player),
-        --on_take = function(inv, listname, index, stack, player),
-        -- Called after the actual action has happened, according to what was
-        -- allowed.
-        -- No return value.
-    }, player:get_player_name())
+        on_move = function(inv, from_list, from_index, to_list, to_index, count, player)
+            save_full_toolbox(inv, player) -- will optimize later
+        end,
+        on_put = function(inv, listname, index, stack, player)
+            save_full_toolbox(inv, player) -- will optimize later
+        end,
+        on_take = function(inv, listname, index, stack, player)
+            save_full_toolbox(inv, player) -- will optimize later
+        end
+    }, pname)
 
-    if not inv_list then
-        mc_toolmenu:set_string(pname, minetest.serialize(inv:get_lists()))
+    -- create a saved toolbox if it does not already exist
+    local toolbox_save = mc_toolmenu:get(pname)
+    if not toolbox_save then
+        mc_toolmenu:set_string(pname, minetest.serialize({tools = {inv = {}, size = 32}}))
+        toolbox_save = mc_toolmenu:get(pname)
     end
-    --local inv = minetest.get_inventory({type = "detached", name = "mc_toolmenu:"..pname}) 
     
-    local existing_lists = inv:get_lists()
-    inv:set_list("tools", {})
-
-    -- debug
-    minetest.chat_send_all(minetest.serialize(existing_lists))
-    minetest.chat_send_all(minetest.serialize(inv:get_lists()))
+    -- load the saved toolbox into MineTest
+    load_saved_toolbox(toolbox, player)
 end)
 
---[[ player inventory snippet
-local inv = minetest.get_inventory({type = "player", name = pname})
-if not inv:get_list("mc_toolmenu:tools") then
-    inv:set_list("mc_toolmenu:tools", {})
-    inv:set_size("mc_toolmenu:tools", 3)
-end
+
+minetest.register_on_leaveplayer(function(player)
+    -- player check
+    if not player:is_player() then return end
+
+    local pname = player:get_player_name()
+    local inv = minetest.get_inventory({type = "detached", name = "mc_toolmenu:"..pname}) 
+
+    -- save to storage for good measure
+    save_full_toolbox(inv, player)
+    -- remove unused detached inventory to free up resources
+    minetest.remove_detached_inventory("mc_toolmenu:"..pname)
+end)
+
+--[[
+-- May be useful for periodically saving data, though an ABM/LBM may be better since this event is fired roughly every 0.1 seconds
+minetest.register_globalstep(function(dtime)
+
+end)
 ]]
