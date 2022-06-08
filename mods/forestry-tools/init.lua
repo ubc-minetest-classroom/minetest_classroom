18414a592f243e77b7a9aaf519a0c28cb88d89ca
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2022-06-08 12:56:01 -0700

    Separate measuring tape functions

--- mods/forestry-tools/init.lua
+++ mods/forestry-tools/init.lua
@@ -1,70 +1,14 @@
 forestry_tools = { path = minetest.get_modpath("forestry_tools") }
 
-local function check_perm(player)
+function check_perm(player)
 	return minetest.check_player_privs(player:get_player_name(), { shout = true })
 end
 
-
-----------------------------------
---   MEASURING TAPE FUNCTIONS   --
-----------------------------------
-
-local function show_measuring_tape(player)
-	if check_perm(player) then
-		local pname = player:get_player_name()
-		return true
-	end
-end
-
-minetest.register_tool("forestry_tools:measuringTape" , {
-	description = "Measuring Tape",
-	inventory_image = "measuring_tape.png",
-	-- Left-click the tool activates the function
-	on_use = function (itemstack, user, pointed_thing)
-        local pname = user:get_player_name()
-		-- Check for shout privileges
-		if check_perm(user) then
-			show_measuring_tape(user)
-		end
-	end,
-	-- Destroy the item on_drop to keep things tidy
-	on_drop = function (itemstack, dropper, pos)
-		minetest.set_node(pos, {name="air"})
-	end,
-})
-
-minetest.register_alias("measuringTape", "forestry_tools:measuringTape")
-measuringTape = minetest.registered_aliases[measuringTape] or measuringTape
-
--- Give the measuring tape to any player who joins with adequate privileges or take it away if they do not have them
-minetest.register_on_joinplayer(function(player)
-    local inv = player:get_inventory()
-    if inv:contains_item("main", ItemStack("forestry_tools:measuringTape")) then
-        -- Player has the measuring tape
-        if check_perm(player) then
-            -- The player should have the measuring tape
-            return
-        else   
-            -- The player should not have the measuring tape
-            player:get_inventory():remove_item('main', "forestry_tools:measuringTape")
-        end
-    else
-        -- Player does not have the measuring tape
-        if check_perm(player) then
-            -- The player should have the measuring tape
-            player:get_inventory():add_item('main', "forestry_tools:measuringTape")
-        else
-            -- The player should not have the measuring tape
-            return
-        end     
-    end
-end)
-
+dofile(forestry_tools.path .. "/measuring_tape.lua")
 
 --------------------------------
 --  RANGEFINDER FUNCTIONS   --
 ---------------------------------
-
 
 local function show_rangefinder(player)
 	if check_perm(player) then
@@ -137,7 +81,7 @@ minetest.register_globalstep(function(dtime)
 		local stackidx=0
 		--first check to see if the user has a compass
 		local wielded_item = player:get_wielded_item():get_name()
-		if string.sub(wielded_item, 0, 12) == "compass:" then
+		if string.sub(wielded_item, 0, 12) == "forestry_tools:compass:" then
 			--if the player is wields a compass, change the wielded image
 			wielded=true
 			stackidx=player:get_wield_index()
@@ -148,7 +92,7 @@ minetest.register_globalstep(function(dtime)
 				--check  entire list since arrays are not sorted 
 				
 				for i,stack in ipairs(player:get_inventory():get_list("main")) do
-					if i<=activewidth and string.sub(stack:get_name(), 0, 12) == "compass:" then
+					if i<=activewidth and string.sub(stack:get_name(), 0, 12) == "forestry_tools:compass:" then
 						activeinv=stack  --store the stack so we can update it later with new image
 						stackidx=i --store the index so we can add image at correct location
 						gotacompass=true
@@ -167,9 +111,9 @@ minetest.register_globalstep(function(dtime)
 
 			--update compass image to point at target
 			if wielded then
-				player:set_wielded_item("compass:"..compass_image)
+				player:set_wielded_item("forestry_tools:compass:"..compass_image)
 			elseif activeinv then
-				player:get_inventory():set_stack("main",stackidx,"compass:"..compass_image)
+				player:get_inventory():set_stack("main",stackidx,"forestry_tools:compass:"..compass_image)
 			end --if wielded elsif activin
 		end --if gotacompass
 	end --for i,player in ipairs(players)
@@ -202,7 +146,7 @@ for i,img in ipairs(images) do
 		if i == 1 then
 				inv = 0
 		end
-		minetest.register_tool("compass:"..(i-1), {
+		minetest.register_tool("forestry_tools:compass:"..(i-1), {
 				description = "ACompass",
 				inventory_image = img,
 				wield_image = img,
---
+++ mods/forestry-tools/measuring_tape.lua
@@ -0,0 +1,55 @@
+
+----------------------------------
+--   MEASURING TAPE FUNCTIONS   --
+----------------------------------
+
+local function show_measuring_tape(player)
+	if check_perm(player) then
+		local pname = player:get_player_name()
+		return true
+	end
+end
+
+minetest.register_tool("forestry_tools:measuringTape" , {
+	description = "Measuring Tape",
+	inventory_image = "measuring_tape.png",
+	-- Left-click the tool activates the function
+	on_use = function (itemstack, user, pointed_thing)
+        local pname = user:get_player_name()
+		-- Check for shout privileges
+		if check_perm(user) then
+			show_measuring_tape(user)
+		end
+	end,
+	-- Destroy the item on_drop to keep things tidy
+	on_drop = function (itemstack, dropper, pos)
+		minetest.set_node(pos, {name="air"})
+	end,
+})
+
+minetest.register_alias("measuringTape", "forestry_tools:measuringTape")
+measuringTape = minetest.registered_aliases[measuringTape] or measuringTape
+
+-- Give the measuring tape to any player who joins with adequate privileges or take it away if they do not have them
+minetest.register_on_joinplayer(function(player)
+    local inv = player:get_inventory()
+    if inv:contains_item("main", ItemStack("forestry_tools:measuringTape")) then
+        -- Player has the measuring tape
+        if check_perm(player) then
+            -- The player should have the measuring tape
+            return
+        else   
+            -- The player should not have the measuring tape
+            player:get_inventory():remove_item('main', "forestry_tools:measuringTape")
+        end
+    else
+        -- Player does not have the measuring tape
+        if check_perm(player) then
+            -- The player should have the measuring tape
+            player:get_inventory():add_item('main', "forestry_tools:measuringTape")
+        else
+            -- The player should not have the measuring tape
+            return
+        end     
+    end
+end)
