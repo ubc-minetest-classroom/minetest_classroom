42c36075119366d460a25deb2dcc159800fe9486
lukasdragon <lukasdragon@live.ca>
2022-06-07 17:21:19 -0700

    Adapted all worldManager commands to a unified command table

--- mods/mc_worldmanager/commands.lua
+++ mods/mc_worldmanager/commands.lua
@@ -3,76 +3,66 @@
 
 local commands = {}
 
-minetest.register_chatcommand("realmNew", {
-    privs = {
-        interact = true,
-    },
-    func = function(name, param)
-        param = param or "Unnamed Realm"
-        local testRealm = Realm:New(param, { x = 20, y = 20, z = 20 })
-        testRealm:CreateGround()
-        testRealm:CreateBarriers()
-        return true, "executed command. New realm has ID: " .. testRealm.ID
-    end,
-})
-
-minetest.register_chatcommand("realmDelete", {
-    params = "Realm ID",
-    privs = {
-        interact = true,
-    },
-    func = function(name, param)
-        local requestedRealm = Realm.realmDict[tonumber(param)]
-        if (requestedRealm == nil) then
-            return false, "Requested realm of ID:" .. param .. " does not exist."
-        end
-
-        requestedRealm:Delete()
-
-        return true, "Deleted realm with ID: " .. param
-    end,
-})
-
-minetest.register_chatcommand("realmList", {
+minetest.register_chatcommand("localPos", {
     privs = {
         interact = true,
     },
     func = function(name, param)
 
-        minetest.chat_send_player(name, "Realm Name : Realm ID")
-        for i, t in pairs(Realm.realmDict) do
-            minetest.chat_send_player(name, t.Name .. " : " .. t.ID)
-        end
 
-        return true
+        local player = minetest.get_player_by_name(name)
+
+        local pmeta = player:get_meta()
+        local realmID = pmeta:get_int("realm")
+
+        local requestedRealm = Realm.realmDict[realmID]
+
+        local position = requestedRealm:WorldToLocalPosition(player:get_pos())
+
+        return true, "Your position in the local space of realm " .. param .. " is x: " .. position.x .. " y: " .. position.y .. " z: " .. position.z
+
 
     end,
 })
+
+commands["new"] = function(name, realmID, requestedRealm, params)
+    requestedRealm:CreateBarriers()
+    local param = params[1] or "Unnamed Realm"
+    local size = param[2]
+    local sizeY = param[3]
+    local testRealm = Realm:New(param, { x = size, y = sizeY, z = size })
+    testRealm:CreateGround()
+    testRealm:CreateBarriers()
+end
+
+commands["delete"] = function(name, realmID, requestedRealm, params)
+    requestedRealm:Delete()
+end
+
+commands["list"] = function(name, realmID, requestedRealm, params)
+    minetest.chat_send_player(name, "Realm Name : Realm ID")
+    for i, t in pairs(Realm.realmDict) do
+        minetest.chat_send_player(name, t.Name .. " : " .. t.ID)
+    end
+
+    return true
+end
+
+commands["info"] = function(name, realmID, requestedRealm, params)
+    local spawn = requestedRealm.SpawnPoint
+    local startPos = requestedRealm.StartPos
+    local endPos = requestedRealm.EndPos
+
+    return true, "Realm " .. param .. " has a spawn point of "
+            .. "x:" .. tostring(spawn.x) .. " y:" .. tostring(spawn.y) .. " z:" .. tostring(spawn.z)
+            .. "; startPos of "
+            .. "x:" .. tostring(startPos.x) .. " y:" .. tostring(startPos.y) .. " z:" .. tostring(startPos.z)
+            .. "; endPos of "
+            .. "x:" .. tostring(endPos.x) .. " y:" .. tostring(endPos.y) .. " z:" .. tostring(endPos.z)
+end
+
 
 ---Test command to display realm information
-minetest.register_chatcommand("realmInfo", {
-    params = "Realm ID",
-    privs = {
-        interact = true,
-    },
-    func = function(name, param)
-        local requestedRealm = Realm.realmDict[tonumber(param)]
-        if (requestedRealm == nil) then
-            return false, "Requested realm of ID:" .. param .. " does not exist."
-        end
-
-        local spawn = requestedRealm.SpawnPoint
-        local startPos = requestedRealm.StartPos
-        local endPos = requestedRealm.EndPos
-
-        return true, "Realm " .. param .. " has a spawn point of "
-                .. "x:" .. tostring(spawn.x) .. " y:" .. tostring(spawn.y) .. " z:" .. tostring(spawn.z)
-                .. "; startPos of "
-                .. "x:" .. tostring(startPos.x) .. " y:" .. tostring(startPos.y) .. " z:" .. tostring(startPos.z)
-                .. "; endPos of "
-                .. "x:" .. tostring(endPos.x) .. " y:" .. tostring(endPos.y) .. " z:" .. tostring(endPos.z)
-    end,
-})
 
 minetest.register_chatcommand("realmTP", {
     params = "Realm ID",
@@ -91,32 +81,9 @@ minetest.register_chatcommand("realmTP", {
     end,
 })
 
-
-
-minetest.register_chatcommand("localPos", {
-    params = "Realm ID",
-    privs = {
-        interact = true,
-    },
-    func = function(name, param)
-        local requestedRealm = Realm.realmDict[tonumber(param)]
-        if (requestedRealm == nil) then
-            return false, "Requested realm of ID:" .. param .. " does not exist."
-        end
-
-        local player = minetest.get_player_by_name(name)
-        local position = requestedRealm:WorldToLocalPosition(player:get_pos())
-
-        return true, "Your position in the local space of realm " .. param .. " is x: " .. position.x .. " y: " .. position.y .. " z: " .. position.z
-
-
-    end,
-})
-
 commands["walls"] = function(name, realmID, requestedRealm, params)
     requestedRealm:CreateBarriers()
 end
-
 
 commands["schematic"] = function(name, realmID, requestedRealm, params)
     if (params[1] == "schematic") then
@@ -136,8 +103,6 @@ commands["setspawn"] = function(name, realmID, requestedRealm, params)
 
     return true, "Updated spawnpoint for realm with ID: " .. param
 end
-
-
 
 minetest.register_chatcommand("realm", {
     params = "Subcommand Realm ID Option",
