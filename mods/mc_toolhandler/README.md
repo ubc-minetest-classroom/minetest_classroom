ad16cdb79c72cda2ebf934020ee2e92edfde0cae
Nanjou <lowie375@gmail.com>
2022-06-14 14:56:53 -0700

    Add support for handling groups of similar items to mc_toolhandler
    * Currently untested, will test with compass tool locally

--- mods/mc_toolhandler/README.md
+++ mods/mc_toolhandler/README.md
@@ -9,11 +9,15 @@ All tools handled by `mc_toolhandler` will automatically be given to players wit
 `mc_toolhandler` manages tools for all mods specified in the `optional_depends` of its `mod.conf` file. A tool will be managed by `mc_toolhandler` if it contains at least one of the following properties in its tool definition:
 
 - `_mc_tool_privs`: A table of privileges required to use the tool, or an empty table if no priviliges are required to use the tool.
-  - Usage:
-    ```mc_tool_privs = {interact = true, fly = true}```
+  - Usage: ```_mc_tool_privs = {interact = true, fly = true}```
 - `_mc_tool_include`: Boolean flag indicating whether `mc_toolhandler` should manage the tool.
   - If `_mc_tool_include = true` and no `_mc_tool_privs` are set, tool usage privileges default to `{teacher = true}`.
   - If `_mc_tool_include = false`, `mc_toolhander` will not manage the tool.
+
+Additional properties need to be set for groups of similar tools (ex. a tool with changing textures registered as multiple tools):
+
+- `_mc_tool_group`: A group name for the tool, used to detect the presence of the tool instead of the tool name.
+  - Usage: ```_mc_tool_group = "modname:generic_tool_name"```
 
 ## Managing tools using `mc_toolhandler`
 
@@ -22,3 +26,4 @@ To add tools to the list of tools handled by `mc_toolhandler`, follow the steps 
 - Add the name of the mod containg the tools you wish to manage (as defined in the mod's `mod.conf` file) to the `optional_depends` line of `mc_toolhandler`'s `mod.conf` file
   - `optional_depends` is a comma-separated list, so ensure that each mod in the list is separated by a comma
 - Define the `_mc_tool_privs` and/or `_mc_tool_include` fields in the tool definition for each tool you want `mc_toolhandler` to manage, as specified above
+- If applicable, define `_mc_tool_group` as specified above
--- mods/mc_toolhandler/init.lua
+++ mods/mc_toolhandler/init.lua
@@ -1,4 +1,4 @@
--- Returns name of list item is in, if it is in player's inventory (or toolbox)
+-- Returns name of list item is in, if it is in player's inventory
 local function get_player_item_location(player, itemstack)
     local inv = player:get_inventory()
     for list,_ in pairs(inv:get_lists()) do
@@ -9,7 +9,22 @@ local function get_player_item_location(player, itemstack)
     return nil
 end
 
--- Registers give/take callbacks for all applicable tools
+-- Returns name of list and index of first item with mc_tool_group in player's inventory, if an item with mc_tool_group is in player's inventory
+local function get_player_item_group_location(player, mc_tool_group)
+    local inv = player:get_inventory()
+    for list,data in pairs(inv:get_lists()) do
+        for i,item in pairs(data) do
+            if item:get_definition()._mc_tool_group == mc_tool_group then
+                return list,i
+            end
+        end
+    end
+    return nil
+end
+
+--- Registers give/take callbacks for a tool
+--- @param tool_name Name of the tool
+--- @param data Tool definition table
 local function register_callbacks(tool_name, data)
     -- Give the tool to any player who joins with adequate privileges or take it away if they do not have them
     minetest.register_on_joinplayer(function(player)
@@ -43,7 +58,6 @@ local function register_callbacks(tool_name, data)
             -- Player should have the tool but does not: give one copy
             player:get_inventory():add_item("main", tool_name)
         end
-
         return true -- continue to next callback
     end)
     
@@ -65,13 +79,72 @@ local function register_callbacks(tool_name, data)
                 list = get_player_item_location(player, stack)
             end
         end
-    
         return true -- continue to next callback
     end)
 end
 
+--- Registers give/take callbacks for all tools in a tool group
+--- @param tool_name Name of the tool to give by default
+--- @param data Tool definition table
+local function register_group_callbacks(tool_name, data)
+    -- Give the tool to any player who joins with adequate privileges or take it away if they do not have them
+    minetest.register_on_joinplayer(function(player)
+        local list,i = get_player_item_group_location(player, data._mc_tool_group)
+
+        if not list and mc_helpers.checkPrivs(player, data._mc_tool_privs) then
+            -- Player should have the tool but does not: give one copy
+            player:get_inventory():add_item("main", tool_name)
+        elseif not mc_helpers.checkPrivs(player, data._mc_tool_privs) then
+            -- Player has the tool but should not: remove all copies
+            while list do
+                player:get_inventory():set_stack(list, i, ItemStack(nil))
+                list,i = get_player_item_group_location(player, data._mc_tool_group)
+            end
+        end
+    end)
+
+    -- Give the tool to any player who is granted adequate privileges
+    minetest.register_on_priv_grant(function(name, granter, priv)
+        -- Check if priv has an effect on the privileges needed for the tool
+        if name == nil or not mc_helpers.tableHas(data._mc_tool_privs, priv) or not minetest.get_player_by_name(name) then
+            return true -- skip this callback, continue to next callback
+        end
+    
+        local player = minetest.get_player_by_name(name)
+        local list,i = get_player_item_group_location(player, data._mc_tool_group)
+
+        if not list and mc_helpers.checkPrivs(player, data._mc_tool_privs) then
+            -- Player should have the tool but does not: give one copy
+            player:get_inventory():add_item("main", tool_name)
+        end
+        return true -- continue to next callback
+    end)
+    
+    -- Take the tool away from anyone who is revoked privileges and no longer has adequate ones
+    minetest.register_on_priv_revoke(function(name, revoker, priv)
+        -- Check if priv has an effect on the privileges needed for the tool
+        if name == nil or not mc_helpers.tableHas(data._mc_tool_privs, priv) or not minetest.get_player_by_name(name) then
+            return true -- skip this callback, continue to next callback
+        end
+    
+        local player = minetest.get_player_by_name(name)
+        local list,i = get_player_item_group_location(player, data._mc_tool_group)
+    
+        if list and not mc_helpers.checkPrivs(player, data._mc_tool_privs) then
+            -- Player has the tool but should not: remove all copies
+            while list do
+                player:get_inventory():set_stack(list, i, ItemStack(nil))
+                list,i = get_player_item_group_location(player, data._mc_tool_group)
+            end
+        end
+        return true -- continue to next callback
+    end)
+end
+
+
 -- Open and read mod.conf file from iterator
-local register_tools_from = {} 
+local register_tools_from = {}
+local registered_tools = {}
 local conf_reader = io.lines(minetest.get_modpath("mc_toolhandler") .. "/mod.conf")
 elem = conf_reader()
 while elem do
@@ -92,8 +165,16 @@ end
 for name,data in pairs(minetest.registered_tools) do
     for _,mod in pairs(register_tools_from) do
         if data._mc_tool_include or data._mc_tool_privs and (data.mod_origin == mod or string.match(name, "^"..mod..":.*")) then
-            -- register give/take callbacks for tool
-            register_callbacks(name, data)
+            -- register give/take callbacks for tool, if not part of a tool group
+            if not data._mc_tool_group then
+                -- standard registration
+                register_callbacks(name, data)
+                table.insert(registered_tools, name)
+            elseif not mc_helpers.tableHas(registered_tools, "group:"..data._mc_tool_group) then
+                -- tool group registration
+                register_group_callbacks(name, data)
+                table.insert(registered_tools, "group:"..data._mc_tool_group)
+            end
         end
     end
 end
