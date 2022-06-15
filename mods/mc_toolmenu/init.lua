ff3472337e3023b4c10a26912b410b973f0e74dd
Nanjou <lowie375@gmail.com>
2022-06-15 13:10:41 -0700

    Begin implementing dynamic toolbox resizing

--- mods/mc_toolmenu/init.lua
+++ mods/mc_toolmenu/init.lua
@@ -7,9 +7,12 @@ sfinv.register_page("mc_toolmenu:tools", {
         local pname = player:get_player_name()
         local formtable = {
             "box[-0.28,-0.30;8.35,4.9;#555555]",
-            "label[0,0;(WIP) Only tools can be stored here!]",
-            "list[current_player;mc_toolmenu:tools;0,0.5;8,4;0]",
-            "listring[]"
+            --"label[0,0;(WIP) Only tools can be stored here!]",
+            "scrollbar[8.8,0;0.2,4;vertical;toolbox_scroll;0]",
+            "scroll_container[0,0;8,4;toolbox_scroll;vertical;]",
+            "list[current_player;mc_toolmenu:tools;0,0;8,4;0]",
+            "listring[]",
+            "scroll_container_end[]"
         }
         return sfinv.make_formspec(player, context, table.concat(formtable, ""), true)
     end
@@ -22,7 +25,7 @@ minetest.register_on_joinplayer(function(player)
     local pname = player:get_player_name()
     local inv = player:get_inventory()
 
-    -- register inventory, if not created
+    -- register toolbox if not already created
     if not inv:get_list("mc_toolmenu:tools") then
         inv:set_list("mc_toolmenu:tools", {})
         inv:set_size("mc_toolmenu:tools", 32)
@@ -56,5 +59,20 @@ minetest.register_allow_player_inventory_action(function(player, action, invento
         end
     else
         return -- ignore
+    end
+end)
+
+minetest.register_on_player_inventory_action(function(player, action, inventory, inv_info)
+    -- initial check
+    list_info = inv_info.listname or (inv_info.to_list == "mc_toolmenu:tools" and inv_info.to_list or inv_info.from_list)
+    if list_info ~= "mc_toolmenu:tools" then
+        return -- toolbox not affected, ignore
+    end
+
+    if not inventory:room_for_item("mc_toolmenu:tools", ItemStack("default:pick_bronze")) then
+        -- Increase size of toolbox if it gets full
+        local current_size = inventory:get_size("mc_toolmenu:tools")
+        --local width = inventory:get_width("mc_toolmenu:tools")
+        inventory:set_size("mc_toolmenu:tools", current_size + 8)
     end
 end)
