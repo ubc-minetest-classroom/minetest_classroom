7172a75c101805b0c7bddbddd40b67f3e802a617
Lukas Olson <lukasdragon@live.ca>
2022-06-20 12:10:45 -0700

    Added a realm permissions whitelist config file

--- mods/mc_worldmanager/realm/realmPrivileges.lua
+++ mods/mc_worldmanager/realm/realmPrivileges.lua
@@ -1,1 +1,42 @@
-Realm.whitelistedPrivs = {fly = true, fast = true, noclip = true, give = true, interact = true, }
+Realm.whitelistedPrivs = {}
+local worldRealmPrivWhitelist = minetest.get_worldpath() .. "\\realmPrivilegeWhitelist.conf"
+local modRealmPrivWhitelist = mc_worldManager.path .. "\\realmPrivilegeWhitelist.conf"
+
+function Realm.SetPrivWhitelist(file)
+    local settings = Settings(file)
+
+    for k, v in pairs(Realm.whitelistedPrivs) do
+        settings:set_bool(tostring(k), v)
+    end
+
+    local success = settings:write()
+    return success
+end
+
+function Realm.GetPrivWhitelist(file)
+    local settings = Settings(file)
+    local names = settings:get_names()
+
+    for index, key in pairs(names) do
+        local value = settings:get_bool(key)
+
+        if (value == true) then
+            Realm.whitelistedPrivs[key] = true
+        end
+    end
+end
+
+function Realm.LoadPrivModDefaults()
+    Realm.GetPrivWhitelist(modRealmPrivWhitelist)
+    Realm.SetPrivWhitelist(worldRealmPrivWhitelist)
+end
+
+if (mc_helpers.fileExists(worldRealmPrivWhitelist)) then
+    Realm.GetPrivWhitelist(worldRealmPrivWhitelist)
+else
+    Debug.log("No realm permissions whitelist found, load mod defaults...")
+    Realm.LoadPrivModDefaults()
+end
+
+Debug.logTable("whitelisted Privs", Realm.whitelistedPrivs)
+
