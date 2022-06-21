7f87083f1fcb68e4fc8939583804c09309614596
lukasdragon <lukasdragon@live.ca>
2022-06-21 12:00:55 -0700

    Refactored Realm:ApplyPermissions location; Added whitelist check when granting a player privileges

--- mods/mc_worldmanager/realm/realmPlayerManagement.lua
+++ mods/mc_worldmanager/realm/realmPlayerManagement.lua
@@ -36,31 +36,3 @@ end
 function Realm:RunTeleportOutFunctions(player)
     self:RunFunctionFromTable(self.PlayerLeaveTable, player)
 end
-
-function Realm:ApplyPermissions(player)
-    local name = player:get_player_name()
-    local pmeta = player:get_meta()
-
-    local privs = minetest.get_player_privs(name)
-
-
-    -- Revoke all privileges
-    for k, v in pairs(privs) do
-        privs[k] = nil
-    end
-
-    -- Add the universal privileges that a player has access to.
-    local defaultPerms = minetest.deserialize(pmeta:get_string("universalPrivs"))
-    for k, v in pairs(defaultPerms) do
-        privs[k] = v
-    end
-
-    -- Add the realm privileges for any given realm.
-    if (self.Permissions ~= nil) then
-        for k, v in pairs(self.Permissions) do
-            privs[k] = v
-        end
-    end
-
-    minetest.set_player_privs(name, privs)
-end
--- mods/mc_worldmanager/realm/realmPrivileges.lua
+++ mods/mc_worldmanager/realm/realmPrivileges.lua
@@ -58,6 +58,36 @@ function Realm:UpdateRealmPrivilege(privilegeTable)
     return true, invalidPrivs
 end
 
+function Realm:ApplyPermissions(player)
+    local name = player:get_player_name()
+    local pmeta = player:get_meta()
+
+    local privs = minetest.get_player_privs(name)
+
+
+    -- Revoke all privileges
+    for k, v in pairs(privs) do
+        privs[k] = nil
+    end
+
+    -- Add the universal privileges that a player has access to.
+    local defaultPerms = minetest.deserialize(pmeta:get_string("universalPrivs"))
+    for k, v in pairs(defaultPerms) do
+        privs[k] = v
+    end
+
+    -- Add the realm privileges for any given realm.
+    if (self.Permissions ~= nil) then
+        for k, v in pairs(self.Permissions) do
+            if (Realm.whitelistedPrivs[k] == true) then
+                privs[k] = v
+            end
+        end
+    end
+
+    minetest.set_player_privs(name, privs)
+end
+
 if (mc_helpers.fileExists(worldRealmPrivWhitelist)) then
     Realm.GetPrivWhitelist(worldRealmPrivWhitelist)
 else
