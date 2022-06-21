dab821c63f66cc6a5910466f2070a4e627739052
lukasdragon <lukasdragon@live.ca>
2022-06-21 15:02:22 -0700

    Added GetRealmFromPlayer method and AddPlayer / RemovePlayer
    GetRealmFromPlayer is intended to simplify determining which realm a player is located in.
    
    Realm:AddPlayer and Realm:RemovePlayer are internal methods used to keep track of the current location of online players.

--- mods/mc_worldmanager/realm/realmPlayerManagement.lua
+++ mods/mc_worldmanager/realm/realmPlayerManagement.lua
@@ -5,6 +5,7 @@ function Realm:TeleportPlayer(player)
         local oldRealm = Realm.realmDict[OldRealmID]
         if (oldRealm ~= nil) then
             oldRealm:RunTeleportOutFunctions(player)
+            oldRealm:RemovePlayer(player)
         end
     end
 
@@ -12,6 +13,7 @@ function Realm:TeleportPlayer(player)
     local spawn = self.SpawnPoint
     player:set_pos(spawn)
 
+    self:AddPlayer(player)
     mc_worldManager.updateHud(player)
 end
 
@@ -34,4 +36,35 @@ end
 
 function Realm:RunTeleportOutFunctions(player)
     self:RunFunctionFromTable(self.PlayerLeaveTable, player)
+end
+
+function Realm:AddPlayer(player)
+    local table = self:get_tmpData("Inhabitants")
+    if (table == nil) then
+        table = {}
+    end
+    table[player:get_player_name()] = true
+    self:set_tmpData("Inhabitants", table)
+end
+
+function Realm:RemovePlayer(player)
+    local table = self:get_tmpData("Inhabitants")
+    if (table == nil) then
+        table = {}
+    end
+    table[player:get_player_name()] = nil
+    self:set_tmpData("Inhabitants", table)
+end
+
+function Realm.ScanForPlayers()
+
+end
+
+
+function Realm.GetRealmFromPlayer(player)
+    local pmeta = player:get_meta()
+    local playerRealmID = pmeta:get_int("realm")
+
+    local realm = Realm.realmDict[playerRealmID]
+    return realm
 end
