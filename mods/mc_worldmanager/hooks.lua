4dda93128c970e0f141676853c67caf80f82a306
lukasdragon <lukasdragon@live.ca>
2022-06-21 15:03:37 -0700

    Simplified the hooks file to use new helper methods. Added player realm tracking on Join and on leave; Added realm saving on shutdown

--- mods/mc_worldmanager/hooks.lua
+++ mods/mc_worldmanager/hooks.lua
@@ -7,28 +7,39 @@ end)
 
 -- When players respawn, we teleport them to the spawnpoint of the realm they belong to
 minetest.register_on_respawnplayer(function(player)
+    local realm = Realm.GetRealmFromPlayer(player)
 
-    local pmeta = player:get_meta()
-    local playerRealmID = pmeta:get_int("realm")
-
-    local spawnRealm = nil
-
-    if (playerRealmID ~= nil) then
-        spawnRealm = Realm.realmDict[playerRealmID]
+    if (realm ~= nil) then
+        player:set_pos(realm.SpawnPoint)
     else
-        spawnRealm = mc_worldManager.GetSpawnRealm()
+        player:set_pos(mc_worldManager.GetSpawnRealm().SpawnPoint)
     end
-
-    player:set_pos(spawnRealm.SpawnPoint)
+    
     return true
 end)
 
 -- When player joins the game, we create their hud
 minetest.register_on_joinplayer(function(player, last_login)
     mc_worldManager.CreateHud(player)
+
+    local realm = Realm.GetRealmFromPlayer(player)
+    if (realm ~= nil) then
+        realm:AddPlayer(player)
+    end
+
 end)
 
 -- When player leave the game, we create their hud
 minetest.register_on_leaveplayer(function(player, timed_out)
     mc_worldManager.RemoveHud(player)
+
+    local realm = Realm.GetRealmFromPlayer(player)
+    if (realm ~= nil) then
+        realm:RemovePlayer(player)
+    end
+
+end)
+
+minetest.register_on_shutdown(function()
+    Realm.SaveDataToStorage()
 end)
