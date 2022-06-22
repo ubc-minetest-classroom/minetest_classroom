6554789949537592af0bb3e1aef4bf18f24baac5
DannyA98 <84751933+DannyA98@users.noreply.github.com>
2022-06-22 15:42:02 -0700

    Update zoom function on wedgeprism

--- mods/forestry-tools/wedgeprism.lua
+++ mods/forestry-tools/wedgeprism.lua
@@ -6,24 +6,44 @@
      })
 
 
-      function update_wedge(player)
-                 local wielding = player:get_wielded_item()
-             local playername = player:get_player_name()
-                local privs = minetest.get_player_privs(playername)
-                if not wielding:is_empty() and wielding:get_name() == "forestry_tools:wedgeprism" then
-                        -- Has prism
-                        if privs.zoom ~= true then
-                                 privs.zoom = true
-                                 minetest.set_player_privs(playername, privs)
-                         end
-                else
-                        -- Does not have prism
-                        if privs.zoom == true then
-                                privs.zoom = nil
-                                minetest.set_player_privs(playername, privs)
-                     end
-                 end
-         end
+minetest.register_privilege("alwayszoom", {})
+
+
+--       function update_wedge(player)
+--                  local wielding = player:get_wielded_item()
+--              local playername = player:get_player_name()
+--                 local privs = minetest.get_player_privs(playername)
+--                 if not wielding:is_empty() and wielding:get_name() == "forestry_tools:wedgeprism" then
+--                         -- Has prism
+--                         if privs.zoom ~= true then
+--                                  privs.zoom = true
+--                                  minetest.set_player_privs(playername, privs)
+--                          end
+--                 else
+--                         -- Does not have prism
+--                         if privs.zoom == true then
+--                                 privs.zoom = nil
+--                                 minetest.set_player_privs(playername, privs)
+--                      end
+--                  end
+--          end
+
+
+
+minetest.register_globalstep(function()
+	for _,player in ipairs(minetest.get_connected_players()) do
+		local name = player:get_player_name()
+		local privs = minetest.get_player_privs(name)
+		if not privs.zoom and player:get_wielded_item():get_name()=="forestry_tools:wedgeprism" then
+			privs.zoom = true
+			minetest.set_player_privs(name, privs)
+		elseif privs.zoom and player:get_wielded_item():get_name()~="forestry_tools:wedgeprism" and not privs.alwayszoom then
+			privs.zoom = nil
+			minetest.set_player_privs(name, privs)
+		end
+	end
+end)
+
 
 
 
