d0758e111235f678966939bc8f942f1ef1482a13
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2022-07-19 17:33:11 -0700

    Further refactor clinometer

--- mods/forestry-tools/clinometer.lua
+++ mods/forestry-tools/clinometer.lua
@@ -5,54 +5,27 @@ local degreePlus0Hud, degreePlus10Hud, degreePlus20Hud, degreeMinus10Hud, degree
 local percentPlus0Hud, percentPlus10Hud, percentPlus20Hud, percentPlus30Hud, percentPlus40Hud = mhud.init(), mhud.init(), mhud.init(), mhud.init(), mhud.init()
 local percentMinus10Hud, percentMinus20Hud, percentMinus30Hud, percentMinus40Hud = mhud.init(), mhud.init(), mhud.init(), mhud.init()
 
--- local measurementHuds = {}
+local measurementHuds = {}
 
--- table.insert(measurementHuds, { hudObject = degreePlus0Hud, string = "degreePlus0", xAlignment = "left", xOffset = -40, yOffset = -4 })
--- table.insert(measurementHuds, { hudObject = degreePlus10Hud, string = "degreePlus10", xAlignment = "left", xOffset = -40, yOffset = -70 })
--- table.insert(measurementHuds, { hudObject = degreePlus20Hud, string = "degreePlus20", xAlignment = "left", xOffset = -40, yOffset = -135 })
--- table.insert(measurementHuds, { hudObject = degreeMinus10Hud, string = "degreeMinus10", xAlignment = "left", xOffset = -40, yOffset = 62 })
--- table.insert(measurementHuds, { hudObject = degreeMinus20Hud, string = "degreeMinus20", xAlignment = "left", xOffset = -40, yOffset = 125 })
+table.insert(measurementHuds, { hudObject = degreePlus0Hud, string = "degreePlus0", xAlignment = "left", xOffset = -40, yOffset = -4, value = 0 })
+table.insert(measurementHuds, { hudObject = degreePlus10Hud, string = "degreePlus10", xAlignment = "left", xOffset = -40, yOffset = -70, value = 0 })
+table.insert(measurementHuds, { hudObject = degreePlus20Hud, string = "degreePlus20", xAlignment = "left", xOffset = -40, yOffset = -135, value = 0 })
+table.insert(measurementHuds, { hudObject = degreeMinus10Hud, string = "degreeMinus10", xAlignment = "left", xOffset = -40, yOffset = 62, value = 0 })
+table.insert(measurementHuds, { hudObject = degreeMinus20Hud, string = "degreeMinus20", xAlignment = "left", xOffset = -40, yOffset = 125, value = 0 })
 
--- local degreeHuds = {
--- 	plus0 = {
--- 		hudObject = degreePlus0Hud,
--- 		title = "degreePlus0",
--- 		xAlignment = "left",
--- 		xOffset = -40,
--- 		yOffset = -4
--- 	},
--- 	plus10 = {
--- 		hudObject = degreePlus10Hud,
--- 		title = "degreePlus10",
--- 		xAlignment = "left",
--- 		xOffset = -40,
--- 		yOffset = -70
--- 	},
--- 	plus20 = {
--- 		hudObject = degreePlus20Hud,
--- 		title = "degreePlus20",
--- 		xAlignment = "left",
--- 		xOffset = -40,
--- 		yOffset = -135
--- 	},
--- 	minus10 = {
--- 		hudObject = degreeMinus10Hud,
--- 		title = "degreeMinus10",
--- 		xAlignment = "left",
--- 		xOffset = -40,
--- 		yOffset = 62
--- 	},
--- 	minus20 = {
--- 		hudObject = degreeMinus20Hud,
--- 		title = "degreeMinus20",
--- 		xAlignment = "left",
--- 		xOffset = -40,
--- 		yOffset = 125
--- 	}
--- }
+table.insert(measurementHuds, { hudObject = percentPlus0Hud, string = "percentPlus0", xAlignment = "right", xOffset = 35, yOffset = -4, value = 0 })
+table.insert(measurementHuds, { hudObject = percentPlus10Hud, string = "percentPlus10", xAlignment = "right", xOffset = 35, yOffset = -43, value = 0 })
+table.insert(measurementHuds, { hudObject = percentPlus20Hud, string = "percentPlus20", xAlignment = "right", xOffset = 35, yOffset = -83, value = 0 })
+table.insert(measurementHuds, { hudObject = percentPlus30Hud, string = "percentPlus30", xAlignment = "right", xOffset = 35, yOffset = -120, value = 0 })
+table.insert(measurementHuds, { hudObject = percentPlus40Hud, string = "percentPlus40", xAlignment = "right", xOffset = 35, yOffset = -160, value = 0 })
+table.insert(measurementHuds, { hudObject = percentMinus10Hud, string = "percentMinus10", xAlignment = "right", xOffset = 35, yOffset = 37, value = 0 })
+table.insert(measurementHuds, { hudObject = percentMinus20Hud, string = "percentMinus20", xAlignment = "right", xOffset = 35, yOffset = 75, value = 0 })
+table.insert(measurementHuds, { hudObject = percentMinus30Hud, string = "percentMinus30", xAlignment = "right", xOffset = 35, yOffset = 115, value = 0 })
+table.insert(measurementHuds, { hudObject = percentMinus40Hud, string = "percentMinus40", xAlignment = "right", xOffset = 35, yOffset = 153, value = 0 })
 
-local function show_measurement_hud(player, hudName, string, xAlignment, xOffset, yOffset)
-	hudName:add(player, string, {
+
+local function show_measurement_hud(player, hudObject, string, xAlignment, xOffset, yOffset)
+	hudObject:add(player, string, {
 		hud_elem_type = "text",
 		text = "",
 		alignment = {x = xAlignment, y = "centre"},
@@ -78,44 +51,18 @@ end
 local function show_all_huds(player)
 	show_bg_hud(player)
 
-	-- for _,hud in ipairs(degreeHuds) do
-	-- 	show_measurement_hud(player, hud.hudObject, hud.title, hud.xAlignment, hud.xOffset, hud.yOffset)
-	-- end
-
-	show_measurement_hud(player, degreePlus0Hud, "degreePlus0", "left", -40, -4)
-	show_measurement_hud(player, degreePlus10Hud, "degreePlus10", "left", -40, -70)
-	show_measurement_hud(player, degreePlus20Hud, "degreePlus20", "left", -40, -135)
-	show_measurement_hud(player, degreeMinus10Hud, "degreeMinus10", "left", -40, 62)
-	show_measurement_hud(player, degreeMinus20Hud, "degreeMinus20", "left", -40, 125)
-
-	show_measurement_hud(player, percentPlus0Hud, "percentPlus0", "right", 35, -4)
-	show_measurement_hud(player, percentPlus10Hud, "percentPlus10", "right", 35, -43)
-	show_measurement_hud(player, percentPlus20Hud, "percentPlus20", "right", 35, -83)
-	show_measurement_hud(player, percentPlus30Hud, "percentPlus30", "right", 35, -120)
-	show_measurement_hud(player, percentPlus40Hud, "percentPlus40", "right", 35, -160)
-	show_measurement_hud(player, percentMinus10Hud, "percentMinus10", "right", 35, 37)
-	show_measurement_hud(player, percentMinus20Hud, "percentMinus20", "right", 35, 75)
-	show_measurement_hud(player, percentMinus30Hud, "percentMinus30", "right", 35, 115)
-	show_measurement_hud(player, percentMinus40Hud, "percentMinus40", "right", 35, 153)
+	for _,hud in ipairs(measurementHuds) do
+		show_measurement_hud(player, hud.hudObject, hud.string, hud.xAlignment, hud.xOffset, hud.yOffset)
+	end
 	HUD_showing = true
 end
 
 local function hide_huds()
 	bgHud:remove_all()
-	degreePlus0Hud:remove_all()
-	degreePlus10Hud:remove_all()
-	degreePlus20Hud:remove_all()
-	degreeMinus10Hud:remove_all()
-	degreeMinus20Hud:remove_all()
-	percentPlus0Hud:remove_all()
-	percentPlus10Hud:remove_all()
-	percentPlus20Hud:remove_all()
-	percentPlus30Hud:remove_all()
-	percentPlus40Hud:remove_all()
-	percentMinus10Hud:remove_all()
-	percentMinus20Hud:remove_all()
-	percentMinus30Hud:remove_all()
-	percentMinus40Hud:remove_all()
+
+	for _,hud in ipairs(measurementHuds) do
+		hud.hudObject:remove_all()
+	end
 	HUD_showing = false
 end
 
@@ -155,76 +102,90 @@ end
 
 local function update_clinometer(player)
 	bg_angle = "0"
-	local degPlus10, degPlus20, degMinus10, degMinus20 = tostring(curr_pitch + 10), tostring(curr_pitch + 20), tostring(curr_pitch - 10), tostring(curr_pitch - 20)
-	local percPlus10, percPlus20, percPlus30, percPlus40 = tostring(curr_percent + 10), tostring(curr_percent + 20), tostring(curr_percent + 30), tostring(curr_percent + 40)
-	local percMinus10, percMinus20, percMinus30, percMinus40 = tostring(curr_percent - 10), tostring(curr_percent - 20), tostring(curr_percent - 30), tostring(curr_percent - 40)
+
+	-- degPlus0, degPlus10, degPlus20, degMinus10, degMinus20
+	measurementHuds[1].value, measurementHuds[2].value, measurementHuds[3].value, measurementHuds[4].value, measurementHuds[5].value = tostring(curr_pitch), tostring(curr_pitch + 10), tostring(curr_pitch + 20), tostring(curr_pitch - 10), tostring(curr_pitch - 20)
+	-- percentPlus0, percentPlus10, percentPlus20, percentPlus30, percentPlus40
+	measurementHuds[6].value, measurementHuds[7].value, measurementHuds[8].value, measurementHuds[9].value, measurementHuds[10].value = tostring(curr_percent), tostring(curr_percent + 10), tostring(curr_percent + 20), tostring(curr_percent + 30), tostring(curr_percent + 40)
+	-- percentMinus10, percentMinus20, percentMinus30, percentMinus40
+	measurementHuds[11].value, measurementHuds[12].value, measurementHuds[13].value, measurementHuds[14].value = tostring(curr_percent - 10), tostring(curr_percent - 20), tostring(curr_percent - 30), tostring(curr_percent - 40)
+
 
 	if (curr_percent + 40) < -1000 then
-		percPlus10, percPlus20, percPlus30, percPlus40 = "∞", "∞", "∞", "∞"
+		-- percentPlus10, percentPlus20, percentPlus30, percentPlus40
+		measurementHuds[7].value, measurementHuds[8].value, measurementHuds[9].value, measurementHuds[10].value = "∞", "∞", "∞", "∞"
 	elseif (curr_percent + 30) < -1000 then
-		percPlus10, percPlus20, percPlus30 = "∞", "∞", "∞"
+		-- percentPlus10, percentPlus20, percentPlus30
+		measurementHuds[7].value, measurementHuds[8].value, measurementHuds[9].value = "∞", "∞", "∞"
 	elseif (curr_percent + 20) < -1000 then
-		percPlus10, percPlus20 = "∞", "∞"
+		-- percentPlus10, percentPlus20
+		measurementHuds[7].value, measurementHuds[8].value = "∞", "∞"
 	elseif (curr_percent + 10) > 1000 or (curr_percent + 10) < -1000 then
-		percPlus10 = "∞"
+		-- percentPlus10
+		measurementHuds[7].value = "∞"
 	end
 
 	if (curr_percent - 40) > 1000 then
-		percMinus10, percMinus20, percMinus30, percMinus40 = "∞", "∞", "∞", "∞"
+		-- percentMinus10, percentMinus20, percentMinus30, percentMinus40
+		measurementHuds[11].value, measurementHuds[12].value, measurementHuds[13].value, measurementHuds[14].value = "∞", "∞", "∞", "∞"
 	elseif (curr_percent - 30) > 1000 then
-		percMinus10, percMinus20, percMinus30 = "∞", "∞", "∞"
+		-- percentMinus10, percentMinus20, percentMinus30
+		measurementHuds[11].value, measurementHuds[12].value, measurementHuds[13].value = "∞", "∞", "∞"
 	elseif (curr_percent - 20) > 1000 then
-		percMinus10, percMinus20 = "∞", "∞"
+		-- percentMinus10, percentMinus20
+		measurementHuds[11].value, measurementHuds[12].value = "∞", "∞"
 	elseif (curr_percent - 10) > 1000 or (curr_percent - 10) < -1000 then
-		percMinus10 = "∞"
+		-- percentMinus10
+		measurementHuds[11].value = "∞"
 	end
 
 	if curr_pitch >= 70 and curr_pitch < 80 then
 		bg_angle = "70"
-		percPlus40 = ""
+		-- percentPlus40
+		measurementHuds[10].value = ""
 	elseif curr_pitch >= 80 and curr_pitch < 89 then
 		bg_angle = "80"
-		percPlus20, percPlus30, percPlus40 = "", "", ""
-		degPlus20 = ""
+		-- percentPlus20, percentPlus30, percentPlus40
+		measurementHuds[8].value, measurementHuds[9].value, measurementHuds[10].value = "", "", ""
+		-- degreePlus20
+		measurementHuds[3].value = ""
 	elseif curr_pitch >= 89 then
 		bg_angle = "90"
-		percPlus10, percPlus20, percPlus30, percPlus40 = "", "", "", ""
-		degPlus10, degPlus20 = "", ""
+		-- percentPlus10, percentPlus20, percentPlus30, percentPlus40
+		measurementHuds[7].value, measurementHuds[8].value, measurementHuds[9].value, measurementHuds[10].value = "", "", "", ""
+		-- degreePlus10, degreePlus20
+		measurementHuds[2].value, measurementHuds[3].value = "", ""
 	elseif curr_pitch <= -70 and curr_pitch > -80 then
 		bg_angle = "70.1"
-		percMinus40 = ""
+		-- percentMinus40
+		measurementHuds[14].value = ""
 	elseif curr_pitch <= -80 and curr_pitch > -89 then
 		bg_angle = "80.1"
-		percMinus20, percMinus30, percMinus40 = "", "", ""
-		degMinus20 = ""
+		-- percentMinus20, percentMinus30, percentMinus40
+		measurementHuds[12].value, measurementHuds[13].value, measurementHuds[14].value = "", "", ""
+		-- degreeMinus20
+		measurementHuds[5].value = ""
 	elseif curr_pitch <= -89 then
 		bg_angle = "90.1"
-		percMinus10, percMinus20, percMinus30, percMinus40 = "", "", "", ""
-		degMinus10, degMinus20 = "", ""
+		-- percentMinus10, percentMinus20, percentMinus30, percentMinus40
+		measurementHuds[11].value, measurementHuds[12].value, measurementHuds[13].value, measurementHuds[14].value = "", "", "", ""
+		-- degreeMinus10, degreeMinus20
+		measurementHuds[4].value, measurementHuds[5].value = "", ""
 	end
 
 	update_hud(player, bgHud, "background", bg_angle) 
 
-	if curr_percent > 1000 or curr_percent < -1000 then
-		update_hud(player, percentPlus0Hud, "percentPlus0", "∞") 
-	else
-		update_hud(player, percentPlus0Hud, "percentPlus0", tostring(curr_percent)) 
+	for i,hud in ipairs(measurementHuds) do
+		if i == 6 then
+			if curr_percent > 1000 or curr_percent < -1000 then
+				update_hud(player, hud.hudObject, hud.string, "∞") 
+			else
+				update_hud(player, hud.hudObject, hud.string, tostring(curr_percent)) 
+			end
+		else
+			update_hud(player, hud.hudObject, hud.string, hud.value)
+		end
 	end
-
-	update_hud(player, percentPlus10Hud, "percentPlus10", percPlus10)
-	update_hud(player, percentPlus20Hud, "percentPlus20", percPlus20)
-	update_hud(player, percentPlus30Hud, "percentPlus30", percPlus30)
-	update_hud(player, percentPlus40Hud, "percentPlus40", percPlus40)
-	update_hud(player, percentMinus10Hud, "percentMinus10", percMinus10)
-	update_hud(player, percentMinus20Hud, "percentMinus20", percMinus20) 
-	update_hud(player, percentMinus30Hud, "percentMinus30", percMinus30) 
-	update_hud(player, percentMinus40Hud, "percentMinus40", percMinus40)
-
-	update_hud(player, degreePlus0Hud, "degreePlus0", tostring(curr_pitch)) 
-	update_hud(player, degreePlus10Hud, "degreePlus10", degPlus10)
-	update_hud(player, degreePlus20Hud, "degreePlus20", degPlus20)
-	update_hud(player, degreeMinus10Hud, "degreeMinus10", degMinus10)
-	update_hud(player, degreeMinus20Hud, "degreeMinus20", degMinus20) 
 end
 
 
