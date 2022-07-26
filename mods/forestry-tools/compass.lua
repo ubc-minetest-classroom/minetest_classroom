2448c8edbbd0800f0cb8b28d61863be6372b4ffd
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2022-07-26 14:57:37 -0700

    Refactor repetitive compass code

--- mods/forestry-tools/compass.lua
+++ mods/forestry-tools/compass.lua
@@ -1,4 +1,5 @@
 local instances = {}
+local needleHud, shedHud, bezelHud, mirrorHud = mhud.init(), mhud.init(), mhud.init(), mhud.init()
 
 minetest.register_on_joinplayer(function(player)
 	instances[player:get_player_name()] = {
@@ -9,7 +10,13 @@ minetest.register_on_joinplayer(function(player)
 		curr_azimuth = 0,
 		curr_needle = "needle_0.png",
 		curr_bezel = "bezel_0.png",
-		curr_shed = "shed_0.png"
+		curr_shed = "shed_0.png",
+		compassOpenHuds = {
+			{ hudObject = mirrorHud, string = "mirror", text = "compass_mirror.png" },
+			{ hudObject = bezelHud, string = "bezel", text = "bezel_0.png" },
+			{ hudObject = shedHud, string = "shed", text = "shed_0.png" },
+			{ hudObject = needleHud, string = "needle", text = "needle_0.png" }
+		}
 	}
 end)
 
@@ -48,59 +55,37 @@ local function show_adjustments_menu(player)
 	minetest.show_formspec(pname, "compass:adjustments_menu", table.concat(adjustments_menu, ""))
 end
 
-local function update_formspec_needle(player)
-	local pname = player:get_player_name()
+local function get_formspec_adjustment(hudVar, stringLen)
 	local preText = "image[4.5,-2;10,10;"
 
-	if string.len(instances[pname].curr_needle) > 12 then 
-		if string.sub(instances[pname].curr_needle, 13, 26) == "^[transformR90" or string.sub(instances[pname].curr_needle, 15, 28) == "^[transformR90" then
+	if string.len(hudVar) > stringLen then
+		if string.sub(hudVar, -2, -1) == "90" then
 			preText = "image[3.75,-1.26;10,10;"
-		elseif string.sub(instances[pname].curr_needle, 13, 27) == "^[transformR180" or string.sub(instances[pname].curr_needle, 15, 29) == "^[transformR180" then
+		elseif string.sub(hudVar, -3, -1) == "180" then
 			preText = "image[4.5,-0.55;10,10;"
-		elseif string.sub(instances[pname].curr_needle, 13, 27) == "^[transformR270" or string.sub(instances[pname].curr_needle, 15, 29) == "^[transformR270" then
+		elseif string.sub(hudVar, -3, -1) == "270" then
 			preText = "image[5.26,-1.26;10,10;"
 		end
 	end
 
-	adjustments_menu[12] = preText .. instances[pname].curr_needle .. "]"
-	show_adjustments_menu(player)
+	return preText
 end
 
-local function update_formspec_bezel(player)
-	local pname = player:get_player_name()
-	local preText = "image[4.5,-2;10,10;"
+local function update_formspec_image(player, adjustNeedle, adjustBezel)
+	local pname = player:get_player_name() 
 
-	if string.len(instances[pname].curr_bezel) > 11 then 
-		if string.sub(instances[pname].curr_bezel, 12, 25) == "^[transformR90" or string.sub(instances[pname].curr_bezel, 14, 27) == "^[transformR90" then
-			preText = "image[3.75,-1.26;10,10;"
-		elseif string.sub(instances[pname].curr_bezel, 12, 26) == "^[transformR180" or string.sub(instances[pname].curr_bezel, 14, 28) == "^[transformR180" then
-			preText = "image[4.5,-0.55;10,10;"
-		elseif string.sub(instances[pname].curr_bezel, 12, 26) == "^[transformR270" or string.sub(instances[pname].curr_bezel, 14, 28) == "^[transformR270" then
-			preText = "image[5.26,-1.26;10,10;"
-		end
+	if adjustNeedle then
+		adjustments_menu[12] = get_formspec_adjustment(instances[pname].curr_needle, 12) .. instances[pname].curr_needle .. "]"
 	end
 
-	adjustments_menu[13] = preText .. instances[pname].curr_bezel .. "]"
+	if adjustBezel then
+		adjustments_menu[13] = get_formspec_adjustment(instances[pname].curr_bezel, 11) .. instances[pname].curr_bezel .. "]"
+	end
+
+	adjustments_menu[14] = get_formspec_adjustment(instances[pname].curr_shed, 10) .. instances[pname].curr_shed .. "]"
+
 	show_adjustments_menu(player)
 end
-
-local function update_formspec_shed(player)
-	local pname = player:get_player_name()
-	local preText = "image[4.5,-2;10,10;"
-
-	if string.len(instances[pname].curr_shed) > 10 then 
-		if string.sub(instances[pname].curr_shed, 11, 24) == "^[transformR90" or string.sub(instances[pname].curr_shed, 13, 26) == "^[transformR90" then
-			preText = "image[3.75,-1.26;10,10;"
-		elseif string.sub(instances[pname].curr_shed, 11, 25) == "^[transformR180" or string.sub(instances[pname].curr_shed, 13, 27) == "^[transformR180" then
-			preText = "image[4.5,-0.55;10,10;"
-		elseif string.sub(instances[pname].curr_shed, 11, 25) == "^[transformR270" or string.sub(instances[pname].curr_shed, 13, 27) == "^[transformR270" then
-			preText = "image[5.26,-1.26;10,10;"
-		end
-	end
-
-	adjustments_menu[14] = preText .. instances[pname].curr_shed .. "]"
-	show_adjustments_menu(player)
-end 
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
 	local pname = player:get_player_name()
@@ -132,8 +117,7 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 
 						minetest.chat_send_player(pname, minetest.colorize("#00ff00", "Compass - magnetic declination set to " .. fields.declination .. "°"))
 
-						minetest.after(0.1, update_formspec_needle, player)
-						minetest.after(0.1, update_formspec_shed, player)
+						minetest.after(0.1, update_formspec_image, player, true, false)
 					end
 				else 
 					minetest.chat_send_player(pname, minetest.colorize("#ff0000", "Compass - magnetic declination must be a number between -90 and 90"))
@@ -153,8 +137,7 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 
 						minetest.chat_send_player(pname, minetest.colorize("#00ff00", "Compass - azimuth set to " .. fields.azimuth .. "°"))
 
-						minetest.after(0.1, update_formspec_bezel, player)
-						minetest.after(0.1, update_formspec_shed, player)
+						minetest.after(0.1, update_formspec_image, player, false, true)
 					end
 				else 
 					minetest.chat_send_player(pname, minetest.colorize("#ff0000", "Compass - azimuth must be a number between 0 and 360"))
@@ -187,53 +170,27 @@ local function show_closed_hud(player)
 	instances[player:get_player_name()].closed_HUD_showing = true
 end
 
-local needleHud = mhud.init()
-local function show_needle_hud(player)
-	needleHud:add(player, "needle", {
-		hud_elem_type = "image",
-		text = "needle_0.png",
-		position = {x = 0.5, y = 0.45}, 
-		scale = {x = 5, y = 5},
-		offset = {x = -4, y = -4},
-		alignment = {x = "centre", y = "centre"}
-	})
+local function show_open_huds(player) 
+	for _,hud in ipairs(instances[player:get_player_name()].compassOpenHuds) do
+		hud.hudObject:add(player, hud.string, {
+			hud_elem_type = "image",
+			text = hud.text,
+			position = {x = 0.5, y = 0.45}, 
+			scale = {x = 5, y = 5},
+			offset = {x = -4, y = -4},
+			alignment = {x = "centre", y = "centre"}
+		})
+	end
 
 	instances[player:get_player_name()].open_HUD_showing = true
 end
 
-local shedHud = mhud.init()
-local function show_shed_hud(player)
-	shedHud:add(player, "shed", {
-		hud_elem_type = "image",
-		text = "shed_0.png",
-		position = {x = 0.5, y = 0.45}, 
-		scale = {x = 5, y = 5},
-		offset = {x = -4, y = -4},
-		alignment = {x = "centre", y = "centre"}
-	})
-end
+local function remove_open_huds(player)
+	for _,hud in ipairs(instances[player:get_player_name()].compassOpenHuds) do
+		hud.hudObject:remove_all()
+	end
 
-local bezelHud = mhud.init()
-local function show_bezel_hud(player)
-	bezelHud:add(player, "bezel", {
-		hud_elem_type = "image",
-		text = "bezel_0.png",
-		position = {x = 0.5, y = 0.45}, 
-		scale = {x = 5, y = 5},
-		offset = {x = -4, y = -4},
-		alignment = {x = "centre", y = "centre"}
-	})
-end
-
-local mirrorHud = mhud.init()
-local function show_mirror_hud(player)
-	mirrorHud:add(player, "mirror", {
-		hud_elem_type = "image",
-		text = "compass_mirror.png",
-		position = {x = 0.5, y = 0.45}, 
-		scale = {x = 5, y = 5},
-		offset = {x = -4, y = -4}
-	})
+	instances[player:get_player_name()].open_HUD_showing = false
 end
 
 minetest.register_tool("forestry_tools:compass" , {
@@ -256,33 +213,22 @@ minetest.register_tool("forestry_tools:compass" , {
 			closedHud:remove_all(player)
 			instances[pname].closed_HUD_showing = false
 
-			show_mirror_hud(player)
-			show_needle_hud(player)
-			show_shed_hud(player)
-			show_bezel_hud(player)
+			show_open_huds(player)
 		else
-			needleHud:remove_all()
-			shedHud:remove_all()
-			bezelHud:remove_all()
-			mirrorHud:remove_all()
-			instances[pname].open_HUD_showing = false
+			remove_open_huds(player)
 		end
 	end,
 
 	-- On right-click
 	on_place = function(itemstack, placer, pointed_thing)
 		if instances[placer:get_player_name()].open_HUD_showing then 
-			update_formspec_needle(placer)
-			update_formspec_bezel(placer)
-			update_formspec_shed(placer)
+			update_formspec_image(placer, true, true)
 		end
 	end,
 
 	on_secondary_use = function(itemstack, player, pointed_thing)
 		if instances[player:get_player_name()].open_HUD_showing then 
-			update_formspec_needle(player)
-			update_formspec_bezel(player)
-			update_formspec_shed(player)
+			update_formspec_image(placer, true, true)
 		end
 	end,
 
@@ -306,10 +252,7 @@ minetest.register_globalstep(function(dtime)
 		if instances[pname].open_HUD_showing then
 			-- Remove HUD when player is no longer wielding the compass
 			if player:get_wielded_item():get_name() ~= "forestry_tools:compass" then
-				needleHud:remove_all()
-				shedHud:remove_all()
-				bezelHud:remove_all()
-				mirrorHud:remove_all()
+				remove_open_huds(player)
 				instances[pname].open_HUD_showing = false
 			else
 				local dir = player:get_look_horizontal()
