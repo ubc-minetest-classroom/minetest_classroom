944610b863f94758d07f3629c77a7dcaa480a229
Lukas Olson <lukasdragon@live.ca>
2022-08-15 15:17:52 -0700

    Got json files to load as biomes

--- mods/frst_bec_biomes/init.lua
+++ mods/frst_bec_biomes/init.lua
@@ -13,19 +13,18 @@ if (removeOtherBiomes) then
 end
 
 local testBiomeTable = {
-    name = "frst_bec_biomes:forest",
-    node_dust = "default:snow",
-    node_top = "default:dirt_with_snow",
+    name = "frst_bec_biomes:test_biome",
+    node_top = "default:dirt_with_grass",
     depth_top = 1,
     node_filler = "default:dirt",
     depth_filler = 3,
     node_stone = "default:stone",
-    node_water_top = "default:dirt",
+    node_water_top = "default:water_source",
     depth_water_top = 10,
-    node_water = "default:dirt",
-    node_riverbed = "default:dirt",
+    node_water = "default:water_source",
+    node_riverbed = "default:water_source",
     depth_riverbed = 2,
-    node_cave_liquid = "default:dirt",
+    node_cave_liquid = "default:water_source",
     y_max = 31000,
     y_min = -31000,
     vertical_blend = 8,
@@ -33,20 +32,30 @@ local testBiomeTable = {
     humidity_point = 50
 }
 
+local dataPath = frstBecBiomes.path .. "\\data\\"
+
 local jsonText = minetest.write_json(testBiomeTable, true)
 
---local f = io.open(frstBecBiomes.path .. "\\data\\" .. "test.json", "wb")
---local content = f:write(jsonText)
---f:close()
+local f = io.open(dataPath .. "testBiome.json", "wb")
+local content = f:write(jsonText)
+f:close()
 
-local files = minetest.get_dir_list(frstBecBiomes.path .. "\\data\\", false)
+local files = minetest.get_dir_list(dataPath, false)
 for k, fileName in pairs(files) do
-    local f = io.open(fileName, "r")
-    local content = f:read("*all")
-    f:close()
+    local ext = string.sub(fileName, -5)
+    if (ext == ".json") then
+        Debug.log("Loading Biome " .. fileName)
+        local f = io.open(dataPath .. fileName, "r")
+        local content = f:read("*all")
+        f:close()
 
-    local biomeTable = minetest.parse_json(content, {})
-    minetest.register_biome(biomeTable)
+        local biomeTable = minetest.parse_json(content, {})
+        minetest.register_biome(biomeTable)
+
+
+    end
+
+
 end
 
 
