9424544338e57783d768c7438296d687956b4257
lukasdragon <lukasdragon@live.ca>
2022-08-15 18:21:57 -0700

    Made the JSON file loading more extendable

--- mods/mc_json_importer/init.lua
+++ mods/mc_json_importer/init.lua
@@ -5,8 +5,6 @@ dofile(json_importer.path .. "\\sampleGen.lua")
 local conf = Settings(json_importer.path .. "/settings.conf")
 local removeOtherBiomes = conf:get_bool("remove_other_biomes", false)
 
-Debug.log("removeOtherBiomes: " .. tostring(removeOtherBiomes))
-
 if (removeOtherBiomes) then
     Debug.log("Removing other biomes")
     minetest.clear_registered_biomes()
@@ -14,8 +12,34 @@ if (removeOtherBiomes) then
     minetest.clear_registered_ores()
 end
 
-local function buildFilePath(rootDirectory)
+-- A table to act as an enum/switch for registering different types of data.
+local dataTypes = {}
+dataTypes["biome"] = function(dataTable)
+    dataTable.jsonType = nil
+    minetest.register_biome(dataTable)
+end
 
+dataTypes["node"] = function(dataTable)
+    dataTable.jsonType = nil
+    minetest.register_node(dataTable.name, dataTable)
+end
+
+dataTypes["craft_item"] = function(dataTable)
+    minetest.register_craftitem(dataTable.name, dataTable)
+end
+
+dataTypes["tool"] = function(dataTable)
+    minetest.register_tool(dataTable.name, dataTable)
+end
+
+dataTypes["craft_recipe"] = function(dataTable)
+    minetest.register_craft(dataTable)
+end
+
+local function buildFilePath(rootDirectory, extension)
+    if (extension == nil) then
+        extension = ".json"
+    end
     local directories = {}
     table.insert(directories, rootDirectory)
 
@@ -27,41 +51,36 @@ local function buildFilePath(rootDirectory)
             table.insert(directories, currentDir .. "\\" .. directory)
         end
 
-        for _, file in pairs(minetest.get_dir_list(currentDir, false)) do
-            table.insert(files, currentDir .. "\\" .. file)
-        end
-    end
-
-    return files
-end
-
-local rootDataDirectory = json_importer.path .. "\\data\\"
-
-local dataDirs = minetest.get_dir_list(rootDataDirectory, true)
-
-for k, dataDirectory in pairs(dataDirs) do
-    local dataDir = rootDataDirectory .. dataDirectory .. "\\"
-
-    -- Load our data in each folder
-    local files = minetest.get_dir_list(dataDir, false)
-    for k, fileName in pairs(files) do
-        local ext = string.sub(fileName, -5)
-        if (ext == ".json") then
-            Debug.log("Loading data " .. fileName)
-            local f = io.open(dataDir .. fileName, "r")
-            local content = f:read("*all")
-            f:close()
-
-            local dataTable = minetest.parse_json(content, {})
-
-            if (dataTable.type == "biome") then
-                Debug.log("Registering biome " .. dataTable.name)
-                minetest.register_biome(dataTable)
-            elseif (dataTable.type == "node") then
-                minetest.register_node(dataTable.name, dataTable)
-            else
-                Debug.log("Unknown data type " .. tostring(dataTable.type) " for " .. tostring(dataDir .. fileName))
+        for _, fileName in pairs(minetest.get_dir_list(currentDir, false)) do
+            local ext = string.sub(fileName, -#extension)
+            if (ext == extension) then
+                table.insert(files, currentDir .. "\\" .. fileName)
             end
         end
+
+        return files
     end
 end
+
+local function loadFile(filePath)
+    local f = io.open(filePath, "r")
+    local content = f:read("*all")
+    f:close()
+
+    local dataTable = minetest.parse_json(content, {})
+
+    if (dataTypes[string.lower(tostring(dataTable.jsonType))] ~= nil) then
+        dataTypes[string.lower(tostring(dataTable.jsonType))](dataTable)
+    else
+        Debug.log("Unknown data type " .. tostring(dataTable.jsonType) " for " .. tostring(filePath))
+    end
+end
+
+local function loadDirectory(rootPath)
+    local files = buildFilePath(rootPath, ".json")
+    for k, file in ipairs(files) do
+        loadFile(file, ".json")
+    end
+end
+
+json_importer.loadDirectory = loadDirectory
