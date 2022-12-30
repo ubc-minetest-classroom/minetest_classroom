4fa3bde771e140be74e846432675da33b7e226cb
pauldpickell <paul.pickell@ubc.ca>
2022-12-29 19:32:33 -0800

    revamped mc_rules GUI, can now be managed

--- mods/mc_rules/init.lua
+++ mods/mc_rules/init.lua
@@ -1,32 +1,71 @@
-server_rules = {}
+mc_rules = {}
+mc_rules.meta = minetest.get_mod_storage()
+mc_rules.data = mc_rules.meta:get_string("rules")
+local priv_table = { server = true }
 
-function server_rules.get_formspec(name)
-    local Header = "Server Rules"
-    local Rule_1 = "1. The University of British Columbia student Code of Conduct applies."
-    local Rule_2 = "2. No harassing: spamming chat, blocking other player's view, entrapping other players."
-    local Rule_3 = "3. No destroying the world or other player's creations."
-    local Rule_4 = "4. Follow the instructions of your professor and teaching assistant at all times."
-    local serveruse = "Your continued use of this server consitutes your agreement with the rules above."
-    local disclaimer = "Server maintained for research and educational purposes by UBC Faculty of Forestry."
-    local admini = "Administered by Prof. Paul Pickell (paul.pickell@ubc.ca) aka 'Prof_Pickell'."
-    local rulescmd = "Type /rules in chat to see these rules again."
+function mc_rules.show_rules_formspec(accepted)
+    -- Check if the rules are set?
+    if mc_rules.meta:get_string("rules") ~= "" then
+        local fs = {}
+        fs[#fs+1] = "formspec_version[6]"
+        fs[#fs+1] = "size[10,13]"
+        fs[#fs+1] = "no_prepend[]"
+        fs[#fs+1] = "bgcolor[#00000000]"
+        fs[#fs+1] = "image[0,0;10,13;background.png]"
+        fs[#fs+1] = "image[0.2,0;9.6,3.84;header.png]"
+        fs[#fs+1] = "style_type[label;font=bold]"
+        fs[#fs+1] = "label[1,3;"..minetest.colorize("#FFF", "MINETEST CLASSROOM").."]"
+        fs[#fs+1] = "style_type[label;font_size=*2.5;font=bold]"
+        fs[#fs+1] = "label[1,3.5;"..minetest.colorize("#FFF", "Server Rules").."]"
+        fs[#fs+1] = "textarea[1,4;8,4;message;;"
+        fs[#fs+1] = mc_rules.meta:get_string("rules")
+        fs[#fs+1] = "]"
+        if accepted then
+            fs[#fs+1] = "image_button_exit[3.15,8.5;3.7,0.74;ok_button.png;ok;"..minetest.colorize("#000", "Okay")..";false;false]"
+        else
+            fs[#fs+1] = "image_button_exit[1.25,8.5;3.7,0.74;yes_button.png;accept;"..minetest.colorize("#000", "Yes\\, game on!")..";false;false]"
+            fs[#fs+1] = "image_button_exit[5.05,8.5;3.7,0.74;no_button.png;reject;"..minetest.colorize("#000", "No thanks\\, bye!")..";false;false]"
+        end
+        fs[#fs+1] = "image[3.3,9.74;3.4,0.28;divider.png]"
+        fs[#fs+1] = "style_type[textarea;font_size=*0.75]"
+        fs[#fs+1] = "textarea[1,10.52;8,0.7;;The Minetest Classroom project was created on the traditional\\, ancestral\\, and unceded territory of the Musqueam People.;]"
+        fs[#fs+1] = "image[1.25,11.24;7.5,1.55;logo.png]"
+        return table.concat(fs, "")
+    else
+        return ""
+    end
+end
 
-    local formspec = {
-        "formspec_version[4]",
-        "size[13,10]",
-        "image[0.375,0.5;6.5,2.04;ubc_forestry_logo.png]",
-        "label[0.375,3;", minetest.formspec_escape(Header), "]",
-        "label[0.375,3.75;", minetest.formspec_escape(Rule_1), "]",
-        "label[0.375,4.5;", minetest.formspec_escape(Rule_2), "]",
-        "label[0.375,5.25;", minetest.formspec_escape(Rule_3), "]",
-        "label[0.375,6;", minetest.formspec_escape(Rule_4), "]",
-        "label[0.375,6.75;", minetest.formspec_escape(disclaimer), "]",
-        "label[0.375,7.5;", minetest.formspec_escape(admini), "]",
-        "label[0.375,8.25;", minetest.formspec_escape(rulescmd), "]",
-        "button_exit[0.375,9;2,0.8;agree;Agree]"
-    }
-
-    return table.concat(formspec, "")
+function mc_rules.show_edit_formspec(message)
+    local fs = {}
+    fs[#fs+1] = "formspec_version[6]"
+    fs[#fs+1] = "size[10,13]"
+    fs[#fs+1] = "no_prepend[]"
+    fs[#fs+1] = "bgcolor[#00000000]"
+    fs[#fs+1] = "image[0,0;10,13;background.png]"
+    fs[#fs+1] = "image[0.5,0;9,3.6;header.png]"
+    fs[#fs+1] = "style_type[label;font=bold]"
+    fs[#fs+1] = "label[1,3;"..minetest.colorize("#FFF", "MINETEST CLASSROOM").."]"
+    fs[#fs+1] = "style_type[label;font_size=*2.5;font=bold]"
+    fs[#fs+1] = "label[1,3.5;"..minetest.colorize("#FFF", "Server Rules").."]"
+    if message then
+        fs[#fs+1] = "textarea[1,4;8,4;message;;"
+        fs[#fs+1] = tostring(message)
+        fs[#fs+1] = "]"
+    elseif mc_rules.meta:get_string("rules") ~= "" then
+        fs[#fs+1] = "textarea[1,4;8,4;message;;"
+        fs[#fs+1] = mc_rules.meta:get_string("rules")
+        fs[#fs+1] = "]"
+    else
+        fs[#fs+1] = "textarea[1,4;8,4;message;;Welcome to your new Minetest Classroom server! This is the default rules message that all players must accept before being allowed to play. You can change this message now by typing your new rules and then pressing the Save Rules button below or you can dismiss this message and change the rules later by typing /rules in the chat. This default message is only visible to the player with the Server privilege.]"
+    end
+    fs[#fs+1] = "image_button_exit[1.25,8.5;3.7,0.74;ok_button.png;dismiss;"..minetest.colorize("#000", "Dismiss")..";false;false]"
+    fs[#fs+1] = "image_button_exit[5.05,8.5;3.7,0.74;save_button.png;save;"..minetest.colorize("#000", "Save Rules")..";false;false]"
+    fs[#fs+1] = "image[3.3,9.74;3.4,0.28;divider.png]"
+    fs[#fs+1] = "style_type[textarea;font_size=*0.75]"
+    fs[#fs+1] = "textarea[1,10.52;8,0.7;;The Minetest Classroom project was created on the traditional\\, ancestral\\, and unceded territory of the Musqueam People.;]"
+    fs[#fs+1] = "image[1.25,11.24;7.5,1.55;logo.png]"
+    return table.concat(fs, "")
 end
 
 minetest.register_chatcommand("rules", {
@@ -34,10 +73,77 @@ minetest.register_chatcommand("rules", {
 	description = "Server rules",
 	func = function(name, param)
 	minetest.after((0.1), function()
-		return minetest.show_formspec(name, "server_rules:rules", server_rules.get_formspec())
+        if mc_helpers.checkPrivs(minetest.get_player_by_name(name), priv_table) then
+            return minetest.show_formspec(name, "mc_rules:edit", mc_rules.show_edit_formspec(nil))
+        else
+            local pmeta = minetest.get_player_by_name(name):get_meta()
+            local pdata = pmeta:get_int("mc_rules")
+            if pdata == nil or pdata == 0 then
+                return minetest.show_formspec(name, "mc_rules:rules", mc_rules.show_rules_formspec(false))
+            else
+                return minetest.show_formspec(name, "mc_rules:rules", mc_rules.show_rules_formspec(true))
+            end
+        end
 	end)
 end})
 
 minetest.register_on_joinplayer(function(player)
-	minetest.show_formspec(player:get_player_name(), "server_rules:rules", server_rules.get_formspec())
+    -- Check if they have already accepted the rules
+    local pmeta = player:get_meta()
+    local pdata = pmeta:get_int("mc_rules")
+    if pdata ~= nil and pdata == 0 then
+        if mc_helpers.checkPrivs(player, priv_table) then
+            minetest.show_formspec(player:get_player_name(), "mc_rules:rules", mc_rules.show_edit_formspec(nil))
+        else
+            minetest.show_formspec(player:get_player_name(), "mc_rules:rules", mc_rules.show_rules_formspec(false))
+        end
+    end
+end)
+
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+    if string.sub(formname, 1, 8) ~= "mc_rules" then
+        return false
+    end
+
+    local wait = os.clock()
+    while os.clock() - wait < 0.05 do
+    end
+
+    -- Menu
+    if formname == "mc_rules:rules" then
+        if fields.accept then
+            local pmeta = player:get_meta()
+            pmeta:set_int("mc_rules", 1)
+            minetest.show_formspec(player:get_player_name(), "mc_rules:rules", mc_rules.show_rules_formspec(true))
+        elseif fields.reject then
+            minetest.kick_player(player:get_player_name(), "You must agree to the rules in order to play on this server.")
+        elseif fields.ok then
+            return true
+        else
+            -- Unhandled exit, check if already agreed, otherwise treat as rejected
+            local pmeta = player:get_meta()
+            local pdata = pmeta:get_int("mc_rules")
+            if pdata and pdata == 1 then
+                return true
+            else
+                if not mc_helpers.checkPrivs(player, priv_table) then
+                    minetest.kick_player(player:get_player_name(), "You must agree to the rules in order to play on this server.")
+                end
+            end
+        end
+    end
+
+    if formname == "mc_rules:edit" then
+        if fields.dismiss then
+            return true
+        elseif fields.save then
+            if mc_helpers.checkPrivs(player, priv_table) then
+                mc_rules.meta:set_string("rules",fields.message)
+                mc_rules.show_edit_formspec(fields.message)
+            end
+        else
+            -- Unhandled exit, treat as dismiss
+            return true
+        end
+    end
 end)
