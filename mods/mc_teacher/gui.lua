6e436650e417ad5c07dbaf23cfde8ef3a2a9af05
pauldpickell <paul.pickell@ubc.ca>
2023-01-04 20:33:33 -0800

    did this fix it

--- mods/mc_teacher/gui.lua
+++ mods/mc_teacher/gui.lua
@@ -387,8 +387,9 @@ function mc_teacher.show_controller_fs(player,tab)
 				return fs
 			end,
 		}
-        -- pmeta:get_string("default_teacher_tab") -- this is causing fatal crash 
+        -- pmeta:get_string("default_teacher_tab") -- this is causing fatal crash
         -- is there a global pmeta somewhere else that makes this nil?
+        -- tab also causes crash
 		table.insert(teacher_formtable, table.concat(tab_map[tab or mc_teacher.fs_context.tab or "1"](), ""))
 		minetest.show_formspec(pname, "mc_teacher:controller_fs", table.concat(teacher_formtable, ""))
 		return true
